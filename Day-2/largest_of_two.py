b777d00e26d72809ee53b9cb64a33bc69a1cd1fa
Prashant <prshantverma8115@gmail.com>
2026-09-09 13:26:59 +0530

    largest of two number

---
+++ Day-2/largest_of_two.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a first number: "))
+b = int(input("Enter a second number: "))
+if a>b:
+    print("A is grater")
+elif b>a:
+    print("B is grater")
+else:
+    print("Both are equals")
