510c49e46ab22db7425d31451878d3ce7eba831a
Prashant <prshantverma8115@gmail.com>
2026-09-09 13:23:18 +0530

    positive_negative

---
+++ Day-2/Positive_Negative _Zero.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number :"))
+if a == 0:
+    print("Zero number")
+elif a >= 1:
+    print("Positive")
+elif a<= 0:
+    print("Negative")
+else:
+    print("not recomended num ")
