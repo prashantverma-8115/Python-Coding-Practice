3c809f4c5ce988a540a7c5adebb4410c34ec1c88
Prashant <prshantverma8115@gmail.com>
2026-09-09 17:59:49 +0530

    add marks grade program

---
+++ Day-2/marks_grade.py
@@ -0,0 +1,13 @@
+marks = int(input("Enter a marks:"))
+if marks>=90 and marks<=100:
+    print("A grade")
+elif marks>=75 and  marks<=89:
+    print("B grade")
+elif marks>=60 and  marks<=74:
+    print("C grade")
+elif marks>=40 and  marks<=59:
+    print("D grade")
+elif marks>=0 and  marks<=39:
+    print("fai1l")    
+else:
+    print("invalid marks")  
