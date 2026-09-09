8838f56c601190909ca4b46673d20604475f21b6
Prashant <prshantverma8115@gmail.com>
2026-09-09 13:32:47 +0530

    Add largest of three number

---
+++ Day-2/largest_of_three.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a first number ="))
+b = int(input("Enter a second number ="))
+c = int(input("Enter a third number ="))
+
+if a>=b and a>=c:
+    print("Enter a number a is largest number")
+elif b>=a and b>=c:
+    print("Enter a b number is a largest number")
+else:
+    print("Enter a c number is a largest number")
